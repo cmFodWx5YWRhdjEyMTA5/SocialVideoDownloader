74e7c2fdb7027223f6a79dbb34d19fad6bf1bc00
muicv1991@gmail.com <Anhyeuem112>
2018-05-17 10:15:21 +0700

    social new

--- app/src/main/java/com/free/videodownloader/Main2Activity.java
+++ app/src/main/java/com/free/videodownloader/Main2Activity.java
@@ -2,6 +2,8 @@ package com.free.videodownloader;
 
 import android.app.Dialog;
 import android.app.DownloadManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.ActivityNotFoundException;
@@ -106,6 +108,23 @@ public class Main2Activity extends AppCompatActivity {
 
         getConfigApp();
     }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("default1", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 
     private void setupViewPager(ViewPager viewPager) {
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -242,7 +261,7 @@ public class Main2Activity extends AppCompatActivity {
 
             @Override
             public void onError(Ad ad, AdError adError) {
-//                Log.d("caomui1",adError.getErrorMessage());
+                Log.d("caomui1",adError.getErrorMessage());
             }
 
             @Override
--- app/src/main/java/com/free/videodownloader/MessageReceiver.java
+++ app/src/main/java/com/free/videodownloader/MessageReceiver.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -17,7 +18,7 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MessageReceiver extends FirebaseMessagingService {
     private static final int REQUEST_CODE = 1;
-    private static final int NOTIFICATION_ID = 6578;
+    private static final int NOTIFICATION_ID = 788899;
 
     public MessageReceiver() {
         super();
@@ -44,13 +45,22 @@ public class MessageReceiver extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, REQUEST_CODE,
                 i, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        Notification notification = new NotificationCompat.Builder(this)
-                .setContentText(msg)
-                .setContentTitle(title)
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
-                .build();
-
+        Notification notification;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notification = new Notification.Builder(this,
+                    "default1")
+                    .setContentText(msg)
+                    .setContentTitle(title)
+                    .setContentIntent(pendingIntent)
+                    .build();
+        } else {
+            notification = new NotificationCompat.Builder(this)
+                    .setContentText(msg)
+                    .setContentTitle(title)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                    .build();
+        }
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         manager.notify(NOTIFICATION_ID, notification);
     }

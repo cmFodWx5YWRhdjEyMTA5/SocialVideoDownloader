79b984cc45a2fa0eb093f96530a85d89e18125e4
muicv91 <muicv1991@gmail.com>
2018-08-30 23:03:57 +0700

    mp4

--- app/src/main/java/com/top1/videodownloader/network/CheckAds.java
+++ app/src/main/java/com/top1/videodownloader/network/CheckAds.java
@@ -1,9 +1,16 @@
 package com.top1.videodownloader.network;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CheckAds {
+    @SerializedName("isShow")
     public int isShow;
+    @SerializedName("isBotClick")
     public int isBotClick;
+    @SerializedName("delayClick")
     public int delayClick;
+    @SerializedName("x")
     public int x;
+    @SerializedName("y")
     public int y;
 }

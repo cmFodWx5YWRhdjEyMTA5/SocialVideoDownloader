601666a89361cfb46ff133648779a85db2d3f420
muicv91 <muicv1991@gmail.com>
2018-08-30 23:05:45 +0700

    mp4

--- app/src/main/java/com/v2social/socialdownloader/network/Site.java
+++ app/src/main/java/com/v2social/socialdownloader/network/Site.java
@@ -4,8 +4,9 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Site {
-
+    @SerializedName("url")
     public String url;
+    @SerializedName("image")
     public String image;
 
 }

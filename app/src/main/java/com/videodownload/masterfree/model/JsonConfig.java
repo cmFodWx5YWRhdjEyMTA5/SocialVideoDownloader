16efb1e2ed85bfcc16d410e6ab03d3412388a0d3
muicv91 <muicv1991@gmail.com>
2018-09-10 23:10:05 +0700

    1103

---
+++ app/src/main/java/com/videodownload/masterfree/model/JsonConfig.java
@@ -0,0 +1,61 @@
+package com.videodownload.masterfree.model;
+
+import java.util.List;
+
+import com.google.gson.annotations.SerializedName;
+
+public class JsonConfig {
+    @SerializedName("idBannerAdmob")
+    public String idBannerAdmob;
+
+    @SerializedName("idBannerFacebook")
+    public String idBannerFacebook;
+
+    @SerializedName("idFullAdmob")
+    public String idFullAdmob;
+
+    @SerializedName("idFullFacebook")
+    public String idFullFacebook;
+
+    @SerializedName("isAccept")
+    public Integer isAccept;
+
+    @SerializedName("newAppPackage")
+    public String newAppPackage;
+
+    @SerializedName("percentAds")
+    public Integer percentAds;
+
+    @SerializedName("percentRate")
+    public Integer percentRate;
+
+    @SerializedName("priorityBanner")
+    public String priorityBanner;
+
+    @SerializedName("priorityFull")
+    public String priorityFull;
+
+    @SerializedName("urlAccept")
+    public List<Site> urlAccept = null;
+
+    @SerializedName("delayService")
+    public Integer delayService;
+
+    @SerializedName("intervalService")
+    public Integer intervalService;
+
+    @SerializedName("idFullService")
+    public String idFullService;
+
+    @SerializedName("delay_retention")
+    public int delay_retention;
+
+    @SerializedName("retention")
+    public int retention;
+
+    @SerializedName("delay_report")
+    public int delay_report;
+    @SerializedName("idFullFbService")
+    public String idFullFbService;
+}
+
---
+++ app/src/main/java/com/videodownload/masterfree/model/Site.java
@@ -0,0 +1,12 @@
+package com.videodownload.masterfree.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Site {
+    @SerializedName("url")
+    public String url;
+    @SerializedName("image")
+    public String image;
+
+}
